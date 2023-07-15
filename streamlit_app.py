13e3fc6a601cc6bbace44fa3d1f212edc5fe0dad
divya-rai07 <139620251+divya-rai07@users.noreply.github.com>
2023-07-15 21:21:37 +0530

    Update streamlit_app.py
    added emojis, new dishes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and blueberry oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard=Boiled Free-Range egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 and blueberry oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard=Boiled Free-Range egg")
+streamlit.text("🥑🍞 Avocado toast")
